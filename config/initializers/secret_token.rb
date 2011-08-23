252c0c1bd4f903e2633700f6b2f358c2fc1d5b4e
Infomarka <infomarka@infomarka-4.(none)>
2011-08-23 13:54:39 -0300

    PP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teste::Application.config.secret_token = 'd8f745bb264539b56478258a9d3cc08ae32c125e9d85c862b89b81567fe2ac11c234da6833dc416e2a82ddb50efff29edd6b4cfb4688a1342cc4bcc1b356ec23'
